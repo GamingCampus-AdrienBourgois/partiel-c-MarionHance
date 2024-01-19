9400cffeb2968da7d463e4b7cfd17dd0da647ac0
unknown <mhance@gamingcampus.local>
2024-01-19 14:20:18 +0100

    2e exo

--- StudentLibrary/Solution2.cpp
+++ StudentLibrary/Solution2.cpp
@@ -1,4 +1,7 @@
 #include "Solution2.h"
+#include <fstream>
+#include <iostream>
+#include <string>
 
 #include <ostream>
 
@@ -8,7 +11,33 @@
 
 float Solution2::GetBalance(const std::string& accountName)
 {
-	return -1.0f;
+	std::ifstream file(accountName + ".txt");
+
+	if (!file) {
+		throw std::logic_error("Compte non trouvé :(");
+	}
+
+	std::string operation;
+	int amount;
+	int balance;
+
+	while (file >> operation >> amount)
+	{
+
+		if (operation == "DEPOSIT") {
+			balance += amount;
+		}
+
+		else if (operation == "WITHDRAW") {
+			balance -= amount; 
+		}
+
+		else {
+			std::cerr << "Opération inconue : " << operation << std::endl;
+		}
+	}
+
+	return balance;
 }
 
 #endif
